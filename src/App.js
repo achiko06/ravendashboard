a71fd29a549721b3444bd8de758fa71f5939f6ad
achiko06 <achiko06@gmail.com>
2022-11-18 16:40:20 +0100

    Navbar v1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import './App.css';
 
-import './App.css'
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const activeMenu = true;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
-        <div className='flex-relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
-
-            <TooltipComponent content='Settings' position='Top'>
-              <button type='button' 
-              className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' 
-              style={{ background: 'blue', borderRadius: '50%'}}>
-                <FiSettings/>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content="Settings"
+              position="Top"
+            >
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
               </button>
+
             </TooltipComponent>
-
           </div>
-
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
@@ -36,7 +50,6 @@ const App = () => {
               <Sidebar />
             </div>
           )}
-
           <div
           className={
             `dark:bg-main-dark-bg  bg-main-bg min-h-screen w-full 
@@ -45,12 +58,11 @@ const App = () => {
               : 'flex-2'
             }`
             }>
-              <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                <Navbar />
-              </div>
-
-              <div>
-              {/*{themeSettings && (<ThemeSettings />)} */}
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {themeSettings && (<ThemeSettings />)}
 
               <Routes>
                 {/* dashboard  */}
@@ -85,7 +97,7 @@ const App = () => {
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,23 +23,23 @@ const Sidebar = () => {
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
-        <div className="flex justify-between items-center">
-          <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-            <SiShopware /> <span>RavenholmDevAdmin</span>
-          </Link>
-          <TooltipComponent content="Menu" position="BottomCenter">
-            <button
-              type="button"
-              onClick={() => {}}
-              style={{ color: currentColor }}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-            >
-              <MdOutlineCancel />
-            </button>
-          </TooltipComponent>
-        </div>
-        <div className="mt-10 ">
-          {links.map((item) => (
+          <div className="flex justify-between items-center">
+            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+              <SiShopware /> <span>Shoppy</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10 ">
+            {links.map((item) => (
               <div key={item.title}>
                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
                   {item.title}
@@ -59,11 +59,10 @@ const Sidebar = () => {
                   </NavLink>
                 ))}
               </div>
-          ))}
-        </div>
+            ))}
+          </div>
         </>
       )}
-
     </div>
   )
 }
