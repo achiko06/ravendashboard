5fbd2905219c953d8d66223e5825843f2a8bef16
achiko06 <achiko06@gmail.com>
2022-11-19 22:36:46 +0100

    charts rest

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -336,8 +336,8 @@ const Ecommerce = () => {
               alt=""
             />
             <div className="mt-8">
-              <p className="font-semibold text-lg">React 18 coming soon!</p>
-              <p className="text-gray-400 ">By Johnathan Doe</p>
+              <p className="font-semibold text-lg">Article header!</p>
+              <p className="text-gray-400 ">By author</p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
                 here. There could be some great info.
